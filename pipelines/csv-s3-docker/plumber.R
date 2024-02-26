183010035a9199d19eab44775f8fe4e372506070
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-02-26 15:46:43 -0800

    use preview images

--- pipelines/csv-s3-docker/plumber.R
+++ pipelines/csv-s3-docker/plumber.R
@@ -5,7 +5,7 @@ library(plumber)
 library(rapidoc)
 library(vetiver)
 b <- board_s3(bucket = "tidymodels-pipeline-example", region = structure("us-west-1", tags = list(type = "scalar")))
-v <- vetiver_pin_read(b, "flights_xgb", version = "20240226T233634Z-c50ac")
+v <- vetiver_pin_read(b, "flights_xgb", version = "20240226T234318Z-ed0ea")
 
 #* @plumber
 function(pr) {
